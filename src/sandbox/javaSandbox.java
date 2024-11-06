47cf9df648e362621d98d8844c1f950de49bad64
SakshamChauhan07 <113049023+SakshamChauhan07@users.noreply.github.com>
2024-11-05 20:44:22 -0500

    Using baisc swing technique to show frame

--- src/sandbox/javaSandbox.java
+++ src/sandbox/javaSandbox.java
@@ -16,13 +16,26 @@ class GFG{
 					e.printStackTrace();
 				}
 		
-       JFrame frame =  new JFrame("Swing");
-       
-       frame.setSize(800,600);
-       
-       
-       frame.setVisible(true);
-       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // Creating ins-tance of JFrame
+        JFrame frame = new JFrame();
+
+        // Creating instance of JButton
+        JButton button = new JButton(" GFG WebSite Click");
+
+        // x axis, y axis, width, height
+        button.setBounds(150, 200, 220, 50);
+
+        // adding button in JFrame
+        frame.add(button);
+
+        // 400 width and 500 height
+        frame.setSize(500, 600);
+
+        // using no layout managers
+        frame.setLayout(null);
+
+        // making the frame visible
+        frame.setVisible(true);
     }
 
 }
